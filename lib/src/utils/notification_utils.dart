b2c953792db8893850120ebf06a93cf064ba4bdb
SalamPS <salamp@salamp.id>
2024-12-26 17:42:32 +0700

    chore(utils): Check mounted before show notification

--- lib/src/utils/notification_utils.dart
+++ lib/src/utils/notification_utils.dart
@@ -2,54 +2,58 @@ import 'package:flutter/material.dart';
 
 class NotificationUtils {
   static void showSuccessNotification(BuildContext context, String message) {
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(
-        content: Row(
-          children: [
-            const Icon(Icons.check_circle, color: Colors.black87),
-            const SizedBox(width: 8),
-            Expanded(
-              child: Text(
-                message, 
-                style: const TextStyle(color: Colors.black87),
-              )
-            ),
-          ],
+    if (context.mounted) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Row(
+            children: [
+              const Icon(Icons.check_circle, color: Colors.black87),
+              const SizedBox(width: 8),
+              Expanded(
+                child: Text(
+                  message, 
+                  style: const TextStyle(color: Colors.black87),
+                )
+              ),
+            ],
+          ),
+          backgroundColor: const Color.fromARGB(255, 230, 250, 252),
+          behavior: SnackBarBehavior.floating,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+          margin: const EdgeInsets.all(8),
+          duration: const Duration(seconds: 2),
         ),
-        backgroundColor: const Color.fromARGB(255, 230, 250, 252),
-        behavior: SnackBarBehavior.floating,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(10),
-        ),
-        margin: const EdgeInsets.all(8),
-        duration: const Duration(seconds: 2),
-      ),
-    );
+      );
+    }
   }
 
-  static void showErrorNotification(BuildContext context, String message) {
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(
-        content: Row(
-          children: [
-            const Icon(Icons.error_outline, color: Colors.black87),
-            const SizedBox(width: 8),
-            Expanded(
-              child: Text(
-                message, 
-                style: const TextStyle(color: Colors.black87),
-              )
-            ),
-          ],
+  void showErrorNotification(BuildContext context, String message) {
+    if (context.mounted) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Row(
+            children: [
+              const Icon(Icons.error_outline, color: Colors.black87),
+              const SizedBox(width: 8),
+              Expanded(
+                child: Text(
+                  message, 
+                  style: const TextStyle(color: Colors.black87),
+                )
+              ),
+            ],
+          ),
+          backgroundColor: const Color.fromARGB(255, 248, 181, 198),
+          behavior: SnackBarBehavior.floating,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+          margin: const EdgeInsets.all(8),
+          duration: const Duration(seconds: 3),
         ),
-        backgroundColor: const Color.fromARGB(255, 248, 181, 198),
-        behavior: SnackBarBehavior.floating,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(10),
-        ),
-        margin: const EdgeInsets.all(8),
-        duration: const Duration(seconds: 3),
-      ),
-    );
+      );
+    }
   }
 } 
