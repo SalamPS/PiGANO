0ac4fb7b3d30400df1ce91c39dd673a28f2c5401
SalamPS <salamp@salamp.id>
2024-12-31 05:19:08 +0700

    feat(notif): Set to floatting to overlay

--- lib/src/utils/notification_utils.dart
+++ lib/src/utils/notification_utils.dart
@@ -3,57 +3,81 @@ import 'package:flutter/material.dart';
 class NotificationUtils {
   static void showSuccessNotification(BuildContext context, String message) {
     if (context.mounted) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          content: Row(
-            children: [
-              const Icon(Icons.check_circle, color: Colors.black87),
-              const SizedBox(width: 8),
-              Expanded(
-                child: Text(
-                  message, 
-                  style: const TextStyle(color: Colors.black87),
-                )
+      final overlay = Overlay.of(context);
+      final overlayEntry = OverlayEntry(
+        builder: (context) => Positioned(
+          top: MediaQuery.of(context).viewInsets.top + 50,
+          left: 10,
+          right: 10,
+          child: Material(
+            color: Colors.transparent,
+            child: Container(
+              padding: const EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                color: const Color.fromARGB(255, 230, 250, 252),
+                borderRadius: BorderRadius.circular(10),
               ),
-            ],
+              child: Row(
+                children: [
+                  const Icon(Icons.check_circle, color: Colors.black87),
+                  const SizedBox(width: 8),
+                  Expanded(
+                    child: Text(
+                      message,
+                      style: const TextStyle(color: Colors.black87),
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ),
-          backgroundColor: const Color.fromARGB(255, 230, 250, 252),
-          behavior: SnackBarBehavior.floating,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(10),
-          ),
-          margin: const EdgeInsets.all(8),
-          duration: const Duration(seconds: 3),
         ),
       );
+
+      overlay?.insert(overlayEntry);
+      Future.delayed(const Duration(seconds: 3), () {
+        overlayEntry.remove();
+      });
     }
   }
 
   static void showErrorNotification(BuildContext context, String message) {
     if (context.mounted) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          content: Row(
-            children: [
-              const Icon(Icons.error_outline, color: Colors.black87),
-              const SizedBox(width: 8),
-              Expanded(
-                child: Text(
-                  message, 
-                  style: const TextStyle(color: Colors.black87),
-                )
+      final overlay = Overlay.of(context);
+      final overlayEntry = OverlayEntry(
+        builder: (context) => Positioned(
+          top: MediaQuery.of(context).viewInsets.top + 50,
+          left: 10,
+          right: 10,
+          child: Material(
+            color: Colors.transparent,
+            child: Container(
+              padding: const EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                color: Colors.redAccent,
+                borderRadius: BorderRadius.circular(10),
               ),
-            ],
+              child: Row(
+                children: [
+                  const Icon(Icons.error, color: Colors.white),
+                  const SizedBox(width: 8),
+                  Expanded(
+                    child: Text(
+                      message,
+                      style: const TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ),
-          backgroundColor: const Color.fromARGB(255, 248, 181, 198),
-          behavior: SnackBarBehavior.floating,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(10),
-          ),
-          margin: const EdgeInsets.all(8),
-          duration: const Duration(seconds: 3),
         ),
       );
+
+      overlay?.insert(overlayEntry);
+      Future.delayed(const Duration(seconds: 3), () {
+        overlayEntry.remove();
+      });
     }
   }
-} 
+}
