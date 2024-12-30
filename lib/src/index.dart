a2805462a6ed9bb401069183742bba7ad6d6303b
SalamPS <salamp@salamp.id>
2024-12-31 06:23:26 +0700

    chore(text): Minor

--- lib/src/index.dart
+++ lib/src/index.dart
@@ -192,7 +192,7 @@ class _EganoWelcomeState extends State<EganoWelcome> {
                         children: [
                           Icon(Icons.key_rounded, color: Colors.white),
                           SizedBox(width: 10),
-                          Text('Try now !', style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
+                          Text('Encrypt yours !', style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
                         ],
                       ),
                     ),
